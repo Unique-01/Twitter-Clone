ff131a1026a10576c6891bcf8a3d3931b1a46739
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-03-08 11:25:35 +0100

    update tweet form ui

--- static/index.js
+++ static/index.js
@@ -5,3 +5,11 @@ function changeDiv(e) {
     target.innerHTML = document.getElementById(section).innerHTML;
 
 }
+
+// var form = document.getElementById('tweetForm');
+
+var seen_by = document.getElementById('id_seen_by');
+seen_by.className='hidden';
+document.getElementById('id_content').onclick = function () {
+    seen_by.className='';
+}
--- tweetapp/forms.py
+++ tweetapp/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Tweet
 
 class TweetForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TweetForm, self).__init__(*args, **kwargs)
+        
+        self.fields['content'].label =""
+        self.fields['seen_by'].label = ""
     class Meta:
         model = Tweet
         fields = ['content','seen_by']
